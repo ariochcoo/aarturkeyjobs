1a21c52393085b078e8e6dbf8ea9bd122e49369a
ariochcoo <melihcan_sa@hotmail.com>
2022-02-01 14:16:05 +0300

    addupdate

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import FirebaseAuthService from "../../FirebaseAuthService";
-import { Form, Card, Button, Container } from "react-bootstrap";
+import { Form, Card, Button, Container, Col, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const LoginPage = function ({ existingUser }) {
@@ -30,43 +30,49 @@ const LoginPage = function ({ existingUser }) {
   return (
     <div>
       <Container>
-        <Card>
-          <Card.Header>
-            <h4>Login </h4>
-          </Card.Header>
-          <Card.Body>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput3"
-              >
-                <Form.Label>Username </Form.Label>
-                <Form.Control
-                  type="email"
-                  name="userName"
-                  value={userName}
-                  onChange={(e) => setUserName(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput4"
-              >
-                <Form.Label>Password </Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
+        <Row>
+          <Col sm={4}></Col>
+          <Col sm={4}>
+            <Card>
+              <Card.Header>
+                <h4>Login </h4>
+              </Card.Header>
+              <Card.Body>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput3"
+                  >
+                    <Form.Label>Username </Form.Label>
+                    <Form.Control
+                      type="email"
+                      name="userName"
+                      value={userName}
+                      onChange={(e) => setUserName(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput4"
+                  >
+                    <Form.Label>Password </Form.Label>
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Form.Group>
 
-              <Button variant="success" type="submit">
-                Login
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
+                  <Button variant="success" type="submit">
+                    Login
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={4}></Col>
+        </Row>
       </Container>
     </div>
   );
